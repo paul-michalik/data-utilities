1516e214b826641ee51784224eb9e5c439eff26c
Paul Michalik <paul.michalik@outlook.com>
2016-11-09 09:53:33 +0100

    add ranges

--- PostProcessed-To-Csv.ps1
+++ PostProcessed-To-Csv.ps1
@@ -13,35 +13,51 @@ if(Test-Path $inputFile) {
     $inputFile
     $outputFile
 
-    Get-Content $inputFile `
-        | Where-Object { $_ -match '^\s{6}Week|^\d{4}\.\d{5}' } `
-        | ForEach-Object {
+    Get-Content $inputFile | 
+        Where-Object { $_ -match '^\s{6}Week|^\d{4}\.\d{5}' } | 
+        ForEach-Object {
             $csv_line = ($_ -replace '(\d{2}) (\d{2}) (\d{1,2}\.\d{5})', '$1-$2-$3').TrimStart() -replace '\s+', ','
             
-            Write-Host "current line:" 
-            $csv_line
+            Write-Host "current line:"
+            Write-Host $csv_line
             
             $csv_line_decimal = ""
             $csv_line_index_ranges = @(0);
             foreach ($match in ($csv_line | Select-String -AllMatches -Pattern '(\d{2})-(\d{2})-(\d{1,2}\.\d{5})').Matches) {                
                 if ($match -and $match.Groups.Count -eq 4) {
-                    Write-Host "Current group: " 
-                    $match.Groups
+                    $pos = New-Object PsCustomObject -Prop @{
+                        Raw = $match.Groups[0]
+                        Deg = [double]$match.Groups[1].Value
+                        Min = [double]$match.Groups[2].Value
+                        Sec = [double]$match.Groups[3].Value
+                    }
+                    Write-Host "Current group:" $pos.Raw.Value "range:" `
+                        $pos.Raw.Index "-" ($pos.Raw.Index + $pos.Raw.Length).ToString()
                     
-                    $csv_line_index_ranges += @($match.Groups[0].Index, $match.Groups[0].Index + $match.Groups[0].Length)
-                    
-                    Write-Host "position deg min sec:" $match.Groups[1].Value $match.Groups[2].Value $match.Groups[3].Value
-                
-                    $position_decimal = `
-                        [double]$match.Groups[1].Value + `
-                            [double]$match.Groups[2].Value/60.0 + `
-                                [double]$match.Groups[3].Value/3600.0;
-            
-                    Write-Host "position decimal:" $position_decimal
+                    $csv_line_index_ranges += @($pos.Raw.Index, ($pos.Raw.Index + $pos.Raw.Length))
+
+                    $position_decimal = $pos.Deg + $pos.Min/60.0 + $pos.Sec/3600.0;
+                                                    
+                    Write-Host "position:" $pos.Deg $pos.Min $pos.Sec "->" $position_decimal
                    
                 }
             }
-        } `
-        #| Out-File $outputFile
+            #add max range
+            $csv_line_index_ranges += $csv_line.Length
+            #remove duplicates
+            $csv_line_index_ranges = $csv_line_index_ranges | select -Unique
+            Write-Host $csv_line_index_ranges
+
+            for($range = 0; $range -lt ($csv_line_index_ranges.Count - 1); $range++) {
+                $index = $csv_line_index_ranges[$range]
+                $size = ($csv_line_index_ranges[($range + 1)] - $csv_line_index_ranges[$range])
+
+                Write-Host "index, size:" $index $size
+                $csv_line_decimal += $csv_line.Substring($index, $size)
+                $csv_line_decimal     
+            }
+            $csv_line_decimal
+        } | 
+        Out-File $outputFile
 
 }
